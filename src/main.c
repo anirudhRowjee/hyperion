3e25eea8ccff4721ac63799dbc9bcaacaaaaee28
anirudhRowjee <ani.rowjee@gmail.com>
2021-06-02 01:26:32 +0530

    Second Revision: Setup minimal SQL compiler and enums with error handling
    This commit involves setting up a small SQL compiler and a virtual
    machine to execute the commands with. As of now, they do nothing, but
    they're built to be expanded for future use.

--- src/main.c
+++ src/main.c
@@ -8,12 +8,31 @@
  */
 
 
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandOutcomes;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} StatementPreparationOutcomes;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
 // Input Buffer to persistently accept user input
 typedef struct {
     char* buffer;
     size_t buffer_length;
     ssize_t input_length;
 } InputBuffer;
+
+typedef struct {
+    StatementType type;
+} Statement;
 
 // utility to print the prompt
 void print_prompt() { printf("MomoDB > ");}
@@ -50,7 +69,7 @@ void read_input(InputBuffer* input_buffer)
         exit(EXIT_FAILURE);
     }
 
-    // remove the trailing newline \n 
+    // remove the trailing newline \n
     input_buffer->input_length = input_length - 1;
     input_buffer->buffer[input_length - 1] = 0;
 }
@@ -60,6 +79,53 @@ void close_input_buffer(InputBuffer* input_buffer)
     // function to free the allocated memory for the Input Buffer
     free(input_buffer->buffer);
     free(input_buffer);
+}
+
+MetaCommandOutcomes do_meta_command(InputBuffer* input_buffer)
+{
+    // as of right now, we only support the exit meta command.
+    if (strcmp(input_buffer->buffer, ".exit") == 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+StatementPreparationOutcomes prepare_statement(InputBuffer* input_buffer, Statement* statement)
+{
+    // this is the simplest SQL compiler to exist
+    // function to prepare a statement based on the command
+    // parse the SQL here?
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+    {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strncmp(input_buffer->buffer, "select", 6) == 0)
+    {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    // if we've reached here, we don't know what command this is
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement)
+{
+    // TODO fill in
+    switch (statement->type)
+    {
+        case (STATEMENT_INSERT):
+            printf("We've just executed an insert statement!\n");
+            break;
+        case (STATEMENT_SELECT):
+            printf("We've just executed a select statement!\n");
+            break;
+    }
 }
 
 int main(int argc, char* argv[])
@@ -74,15 +140,38 @@ int main(int argc, char* argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        // handle meta-command case
+        if (input_buffer->buffer[0] == '.')
         {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
+            switch(do_meta_command(input_buffer))
+            {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecongized command %s\n", input_buffer->buffer);
+                    continue;
+            }
         }
-        else
+
+        // prepare a Statement to execute
+        Statement exec_statement;
+
+        // parse the statement into internal representation
+        switch (prepare_statement(input_buffer, &exec_statement))
         {
-            printf("Unrecognized Command '%s'. \n", input_buffer->buffer);
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf(
+                        "Unrecognized Keyword at the start of '%s'\n",
+                        input_buffer->buffer
+                        );
+                continue;
         }
+
+        // finally, execute the statement
+        execute_statement(&exec_statement);
+        printf("Executed!\n");
     }
 
     return 0;
