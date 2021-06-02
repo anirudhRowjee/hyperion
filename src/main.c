26d6465f3acbb6aafd7027400906105d428f794e
anirudhRowjee <ani.rowjee@gmail.com>
2021-06-02 11:37:54 +0530

    Added In-Memory Storage with Page Model
    This commit adds in-memory storage and insertion support. The current
    iteration leaks memory as the table isn't free-d upon exit.

--- src/main.c
+++ src/main.c
@@ -2,10 +2,26 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdint.h>
 
 /*
  * MomoDB - A Simple SQLite clone
+ *
+ * As of now, There exists only a single table
+ * ---------------------------------------------
+ *  id          integer
+ *  username    varchar(32)
+ *  email       varchar(255)
+ *  -------------------------------------------
+ *
+ *  MomoDB supports Insertion and Read operations.
  */
+
+
+// define some constants
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+#define TABLE_MAX_PAGES 100
 
 
 typedef enum {
@@ -15,13 +31,19 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
-    PREPARE_UNRECOGNIZED_STATEMENT
+    PREPARE_UNRECOGNIZED_STATEMENT,
+    PREPARE_SYNTAX_ERROR
 } StatementPreparationOutcomes;
 
 typedef enum {
     STATEMENT_INSERT,
     STATEMENT_SELECT
 } StatementType;
+
+typedef enum {
+    EXECUTE_TABLE_FULL,
+    EXECUTE_SUCCESS
+} ExecuteResult;
 
 // Input Buffer to persistently accept user input
 typedef struct {
@@ -30,13 +52,105 @@ typedef struct {
     ssize_t input_length;
 } InputBuffer;
 
+// this is a row of our table
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+
+// define field constant sizes
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+// define attribute sizes
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+
+// define memory offsets for easy serialization
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+// table definitions and limits
+
+const uint32_t PAGE_SIZE = 4096; 
+// 4Kb as most operating systems size pages at 4Kb
+// this means that pages won't be broken up by the operating system
+// as 4Kb is the virtual memory size of the system
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct {
+    uint32_t row_count;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
+
+
+// function to create a new table
+Table* new_table()
+{
+    Table* table = malloc(sizeof(Table));
+    // initialize all values to zero or null
+    table->row_count = 0;
+    // initialize blank pages
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++)
+    {
+        table->pages[i] = NULL;
+    }
+    return table;
+}
+
+void free_table(Table* table)
+{
+    // free all memory allocated for the table
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++)
+    {
+        free(table->pages[i]);
+    }
+    free(table);
+}
+
+void* row_slot(Table* table, uint32_t row_number)
+{
+    // get a slot in the table 
+
+    // rows are stored sequentially in pages, so we find the page
+    // index based on the count of the row
+    uint32_t page_number = row_number / ROWS_PER_PAGE;
+    // initialize pointer to page memory
+    void* page = table->pages[page_number];
+
+    // in the event the page doesn't exist, we allocate it
+    if (page == NULL)
+    {
+        page = table->pages[page_number] = malloc(PAGE_SIZE);
+    }
+
+    // find the "index" of the row in the page
+    uint32_t row_offset = row_number % ROWS_PER_PAGE;
+    // find the byte offset for the row
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+
+    // return the exact memory location of the new row
+    return page + byte_offset;
+}
+
 typedef struct {
     StatementType type;
+    Row row_to_insert;
 } Statement;
 
 // utility to print the prompt
 void print_prompt() { printf("MomoDB > ");}
 
+// utility to print a row
+void print_row(Row* row)
+{
+    printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
 
 InputBuffer* new_input_buffer()
 {
@@ -102,6 +216,20 @@ StatementPreparationOutcomes prepare_statement(InputBuffer* input_buffer, Statem
     if (strncmp(input_buffer->buffer, "insert", 6) == 0)
     {
         statement->type = STATEMENT_INSERT;
+        // given that we're constrained to a single table, it's easy for us to
+        // expect the input format and this write it to memory.
+        int arg_count = sscanf(
+            input_buffer->buffer,
+            "insert %d %s %s",
+            &(statement->row_to_insert.id),
+            statement->row_to_insert.username,
+            statement->row_to_insert.email
+        );
+        if (arg_count < 3)
+        {
+            // something is off
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if (strncmp(input_buffer->buffer, "select", 6) == 0)
@@ -113,23 +241,78 @@ StatementPreparationOutcomes prepare_statement(InputBuffer* input_buffer, Statem
     // if we've reached here, we don't know what command this is
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
+// serialization and deserialization for the rows
+void serialize_row(Row* source, void* destination)
+{
+    // copy Y size data located at the second pointer to the first
+    // pointer at a specific offset
+    // printf("Before Copying (%d %s %s)\n", source->id, source->username, source->email);
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+    // at this point of time, all our struct data has been placed at the location
+    // we want it to be at, which will be a page.
+}
 
-void execute_statement(Statement* statement)
+void deserialize_row(void* source, Row* destination)
+{
+    // same thing as serializer, just reverse - we move data from the 
+    // page to the struct.
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+    // printf("Deserialized Row: (%d %s %s)\n", destination->id, destination->username, destination->email);
+}
+
+
+ExecuteResult execute_insert(Statement* statement, Table* table)
+{
+    // make sure the table isn't full
+    if (table->row_count >= TABLE_MAX_ROWS)
+    {
+        return EXECUTE_TABLE_FULL;
+    }
+    else
+    {
+        // get the row we need to insert from the statement parser
+        Row* row_to_insert = &(statement->row_to_insert);
+        // serialize the row and write it to the location in the table
+        serialize_row(row_to_insert, row_slot(table, table->row_count));
+        // increment the number of rows
+        table->row_count += 1;
+        return EXECUTE_SUCCESS;
+    }
+}
+
+ExecuteResult execute_select(Statement* statement, Table* table)
+{
+    // create a new temporary location to hold the row
+    Row row;
+    for (uint32_t i = 0; i < table->row_count; i++)
+    {
+        // copy from memory to temporary
+        deserialize_row(row_slot(table, i), &row);
+        print_row(&row);
+    }
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_statement(Statement* statement, Table* table)
 {
     // TODO fill in
     switch (statement->type)
     {
         case (STATEMENT_INSERT):
-            printf("We've just executed an insert statement!\n");
-            break;
+            return execute_insert(statement, table);
         case (STATEMENT_SELECT):
-            printf("We've just executed a select statement!\n");
-            break;
+            return execute_select(statement, table);
     }
 }
 
 int main(int argc, char* argv[])
 {
+    // initialize the table
+    Table* table = new_table();
     // initialize the new input buffer to accept the commands
     // Since this persists, we use it throughout the lifetime of the application
     InputBuffer* input_buffer = new_input_buffer();
@@ -161,6 +344,8 @@ int main(int argc, char* argv[])
         {
             case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_SYNTAX_ERROR):
+                printf("Syntax Error: Could not Parse Statement\n");
             case (PREPARE_UNRECOGNIZED_STATEMENT):
                 printf(
                         "Unrecognized Keyword at the start of '%s'\n",
@@ -170,8 +355,17 @@ int main(int argc, char* argv[])
         }
 
         // finally, execute the statement
-        execute_statement(&exec_statement);
-        printf("Executed!\n");
+        switch(execute_statement(&exec_statement, table))
+        {
+            case (EXECUTE_SUCCESS):
+                printf("Executed\n");
+                break;
+            case (EXECUTE_TABLE_FULL):
+                printf("Error: The Table is Full!\n");
+                break;
+        }
+
+        // printf("Executed!\n");
     }
 
     return 0;
